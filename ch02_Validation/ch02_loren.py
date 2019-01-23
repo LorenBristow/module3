21ba8dbb20e4a745e52f462802d024f09a3869a6
Loren Bristow <lorendup@gmail.com>
2019-01-23 14:42:24 +0000

    updated task 2 with error handling

--- ch02_Validation/ch02_loren.py
+++ ch02_Validation/ch02_loren.py
@@ -20,18 +20,24 @@ Created on Wed Jan 23 09:30:36 2019
 #    
 
 
-###TASK 2##
+###TASK 2###
 choice = int(input("What is your choice? "))
-while choice < 1 or choice > 3:
-    choice = int(input("What is your choice? "))
-print('***choice****')
-print('1. Display my name')
-print('2. Display my age')
-print('3. Display my city')
-print("What is your choice? {}".format(choice))
-if choice == 1:
-    print('Ms Wu')
-elif choice == 2:
-    print('5 years old')
-elif choice == 3:
-    print('London')
+while True:
+    try:
+        while choice < 1 or choice > 3:
+            choice = int(input("What is your choice? "))
+            #break
+    except ValueError:
+        print("please try another number")
+    print('***choice****')
+    print('1. Display my name')
+    print('2. Display my age')
+    print('3. Display my city')
+    print("What is your choice? {}".format(choice))
+    if choice == 1:
+        print('Ms Wu')
+    elif choice == 2:
+        print('5 years old')
+    elif choice == 3:
+        print('London')
+    break
